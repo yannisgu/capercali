09b996d2a668840947b662b916bdd99428b068d7
Yannis Guedel <yannis.guedel@unic.com>
2014-09-01 22:15:45 +0200

    added add  buttons

--- src/Capercali.WPF/UserControls/EventConfiguration.xaml.cs
+++ src/Capercali.WPF/UserControls/EventConfiguration.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Capercali.Entities;
+using Capercali.WPF.ViewModel;
+using Capercali.WPF.ViewModel.EventConfiguration;
+using ReactiveUI;
 
 namespace Capercali.WPF.UserControls
 {
@@ -20,9 +25,50 @@ namespace Capercali.WPF.UserControls
     /// </summary>
     public partial class EventConfiguration : UserControl
     {
+        public EventConfigurationViewModel ViewModel { get; set; }
+
         public EventConfiguration()
         {
             InitializeComponent();
+
+            AddCourseButton.Command =  AddCourse = new ReactiveCommand();
+            AddCourse.Subscribe(_ =>
+            {
+                var item = new CourseViewModel();
+                ViewModel.Courses.Add(item);
+                var thisItem = CoursesDataGrid.Items[CoursesDataGrid.Items.Count - 1];
+                var cellInfo = new DataGridCellInfo(thisItem, CoursesDataGrid.Columns[0]);
+
+                var cell = (DataGridCell)cellInfo.Column.GetCellContent(cellInfo.Item).Parent;
+                cell.Focus();
+                ((DataGridRow)cell.Parent).IsSelected = true;
+
+                CoursesDataGrid.BeginEdit();
+            });
+
+            DataContextChanged += EventConfiguration_DataContextChanged;
+
+            
         }
+
+        void EventConfiguration_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            
+            ViewModel = DataContext as EventConfigurationViewModel;
+            if (ViewModel != null)
+            {
+                var cannAddControl = ViewModel.WhenAny(y => y.SelectedCourse, y => y.Value != null);
+                AddControlButton.Command = AddControl = new ReactiveCommand(cannAddControl);
+            }
+        }
+
+        public ReactiveCommand AddControl { get; set; }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public ReactiveCommand AddCourse { get; set; }
     }
 }
